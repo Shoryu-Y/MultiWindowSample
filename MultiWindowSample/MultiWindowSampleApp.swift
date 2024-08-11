4f4d9ad331947701b8f8e9a6ce5d11c096aa7965
Shoryu Yamamoto <tatsubee@pixiv.co.jp>
2024-08-12 01:47:48 +0900

    Initial Commit

---
+++ MultiWindowSample/MultiWindowSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiWindowSampleApp.swift
+//  MultiWindowSample
+//
+//  Created by tatsubee on 2024/08/12.
+//
+
+import SwiftUI
+
+@main
+struct MultiWindowSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
